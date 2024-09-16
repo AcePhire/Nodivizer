b835685e5158b974d7e32d6ef70df10dfd397a08
AcePhire <acephire27@gmail.com>
2024-09-16 17:10:09 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/src`));
+
+app.post("/graph/save/:filename", (req, res) => {
+	let filename = req.params.filename;
+	let graph = req.body;
+	
+	fs.writeFile(`${__dirname}/${filename}`, JSON.stringify(graph), (error) => {
+		if (error) {
+			console.log(error);
+		}
+	});;
+});
+
+app.get("/graph/load/:filename", (req, res) => {
+	let filename = req.params.filename;
+	
+	fs.readFile(`${__dirname}/${filename}`, "utf8", (error, data) => {
+		res.setHeader('Content-Type', 'application/json');
+		res.json(data);
+
+	});
+});
+
+app.get("/", (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
---
+++ src/js/index.js
@@ -0,0 +1,168 @@
+let nodes = [] //{data: {id: ""}}
+let edges = [] //{data: {source: "", target: ""}}
+let selectedNodei = null;
+
+
+var cy = cytoscape();
+
+function updateGraph(cy) {
+	cy = cytoscape({
+		container: document.getElementById('cy'),
+
+		elements: {
+			nodes: nodes,
+			edges: edges
+		},
+
+		style: [
+			{
+				selector: 'node',
+				style: {
+					'background-color': '#b3b3b3',
+					'color': '#b3b3b3',
+					'label': 'data(id)',
+					'active-bg-color': '#8a5cf5'
+				}
+			},
+			{
+				selector: 'node:active',
+				style: {
+					'background-color': '#8a5cf5',
+				}
+			},
+			{
+				selector: 'node[id="+"]',
+				style: {
+					'text-valign': 'center',
+					'text-halign': 'center',
+					'text-margin-y': '1.5px',
+
+					'width': '10px',
+					'height': '10px',
+
+					'color': '#373737',
+					'background-color': '#b3b3b3',
+					'background-opacity': '0.8'
+				}
+			},
+			{
+				selector: 'edge',
+				style: {
+					'width': 3,
+					'line-color': '#373737',
+					'curve-style': 'bezier'
+      				}
+			},
+		],
+
+		layout: {
+			name: 'cola',
+			infinite: true
+		}
+	});
+
+	cy.on("select", "node", function(evt){
+                var node = evt.target;
+		
+		if (node.id() != "+"){
+			selectedNode = node.id();
+			
+			var container = document.getElementById("add-node-container")
+			container.style.display = "none";
+
+			x = node.position("x");
+			y = node.position("y");
+
+			cy.add ([
+				{ group: "nodes", data: {id: "+"}, position: {x: x, y: y+40}}
+			]);
+		}else{
+			var container = document.getElementById("add-node-container");
+			container.style.display = "block";
+
+			container.dataset.parent = selectedNode;
+		}
+        })
+
+	cy.on("unselect", "node", function(evt){
+		cy.remove("node[id='+']");
+	});
+}
+
+function updateNodes(data, filename) {
+	nodes.push({data: data});
+}
+
+function updateEdges(source, target, filename) {
+	edges.push({data: {source: source, target: target}});
+}
+
+function saveGraph(filename){
+	$.ajax({
+		type: "post",
+		url: `/graph/save/${filename}`,
+		data: JSON.stringify({nodes: nodes, edges: edges}),
+		dataType: "json",
+		contentType: "application/json; charset=utf-8"
+	});
+}
+
+function loadGraph(filename) {
+	$.ajax({
+		type: "get",
+		url: `/graph/load/${filename}`,
+		dataType: "json",
+		success: function(data) {
+			let d = JSON.parse(data);
+			nodes = d["nodes"];
+			edges = d["edges"];
+			updateGraph(cy);
+		}
+	});
+}
+
+$(document).ready(function(){
+	let file = "a.json";
+	loadGraph(file);
+	$("#add-node-attr").click(function () {
+		$("#add-node-container .top-container .inputs-container").append(`
+			<div class="node-attr">
+				<input class="attr-key" type="text" placeholder="Key">
+				
+				<input class="attr-val" type="text" placeholder="Value">
+			</div>
+		`);
+	});
+
+	$("#add-node").click(function () {
+		data = {};
+
+		let id = $("#add-node-container .top-container .inputs-container .node-id").val();
+		if (id != null && id != ""){
+			data["id"] = id;
+		
+
+			$("#add-node-container .top-container .inputs-container .node-attr").each(function () {
+				let key = $(this).find(".attr-key").val();
+				let value = $(this).find(".attr-val").val();
+			
+				if (key != null && key != "" && value != null && value != ""){
+					data[key] = value;
+				}
+			});
+			
+			let container = document.getElementById("add-node-container");
+			let source = container.dataset.parent
+
+			updateNodes(data, file);
+			updateEdges(source, id, file);
+			updateGraph();
+			saveGraph(file);
+
+			container.dataset.parent = null;
+			container.style.display = "none";
+
+		}
+		
+	});
+});
